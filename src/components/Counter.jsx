01b2edf37f9a0cf6ccd7c57c76e646b456d9c1df
horlayeanca <adelodunfaruq@gmail.com>
2022-11-05 22:38:23 +0100

    .

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -18,9 +18,9 @@ function Counter() {
   const [count, dispatch] = useReducer(countReducer, 0);
 
   return (
-    <div className="">
+    <div className="bg-neutral-200 w-full h-screen flex items-center">
       <div className="flex flex-col p-4 max-w-lg h-96 mx-auto rounded-xl w-full justify-center items-center bg-slate-400">
-        <h1 className="text-3xl">Count: {count}</h1>
+        <h1 className="text-3xl pb-3">Count: {count}</h1>
         <div className="flex flex-row flex-wrap flex-initial p-4 justify-around">
           <button
             className="bg-green-300 p-3 rounded-lg shadow-md hover:bg-slate-600 duration-300 hover:scale-105"

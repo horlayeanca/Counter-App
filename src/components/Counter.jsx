ddd13ace254b1b79e8bf5914bef1a3ea3da8e64f
horlayeanca <adelodunfaruq@gmail.com>
2022-11-05 22:13:03 +0100

    counter-app

---
+++ src/components/Counter.jsx
@@ -0,0 +1,62 @@
+import React, { useReducer } from "react";
+
+function countReducer(initialState = 0, action) {
+  if (action === "increment") {
+    return initialState + 1;
+  } else if (action === "decrement" && initialState > 0) {
+    return initialState - 1;
+  } else if (action === "reset") {
+    return 0;
+  } else if (action === "setValue") {
+    return;
+  } else {
+    return initialState;
+  }
+}
+
+function Counter() {
+  const [count, dispatch] = useReducer(countReducer, 0);
+
+  return (
+    <div className="">
+      <div className="flex flex-col p-4 max-w-lg h-96 mx-auto rounded-xl w-full justify-center items-center bg-slate-400">
+        <h1 className="text-3xl">Count: {count}</h1>
+        <div className="flex flex-row flex-wrap flex-initial p-4 justify-around">
+          <button
+            className="bg-green-300 p-3 rounded-lg shadow-md hover:bg-slate-600 duration-300 hover:scale-105"
+            onClick={() => dispatch("increment")}
+          >
+            increment
+          </button>
+          <button
+            className="bg-green-300 p-3 rounded-lg shadow-md hover:bg-slate-600 duration-300 hover:scale-105"
+            onClick={() => dispatch("decrement")}
+          >
+            decrement
+          </button>
+          <button
+            className="bg-green-300 p-3 rounded-lg shadow-md hover:bg-slate-600 duration-300 hover:scale-105"
+            onClick={() => dispatch("reset")}
+          >
+            reset
+          </button>
+          <div className="mt-8">
+            <input
+              className="p-3 rounded-lg mx-3"
+              type="number"
+              onChange={(e) => dispatch("setValue")}
+            />
+            <button
+              className="bg-green-300 p-3 rounded-lg shadow-md hover:bg-slate-600 duration-300 hover:scale-105"
+              onClick={(e) => dispatch()}
+            >
+              setValue
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Counter;

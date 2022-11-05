08011ef68a4facbf51f056724c9f61df2d29a147
horlayeanca <adelodunfaruq@gmail.com>
2022-11-06 00:27:49 +0100

    added react router dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
+import { BrowserRouter } from "react-router-dom";
 import Counter from "./components/Counter";
 import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <div>
-      <NavBar />
-      <Counter />
-    </div>
+    <BrowserRouter>
+      <div>
+        <NavBar />
+        <Counter />
+      </div>
+    </BrowserRouter>
   );
 }
 

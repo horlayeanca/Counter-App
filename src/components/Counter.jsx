69cffd3247593d9272a412905c2334c23e5623ff
horlayeanca <adelodunfaruq@gmail.com>
2023-01-01 20:09:51 +0100

    updated useReducer.

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -2,37 +2,50 @@ import React, { useReducer, useState } from "react";
 import { Helmet } from "react-helmet-async";
 import { Link, Outlet } from "react-router-dom";
 
-function countReducer(initialState = 0, action) {
-  if (action === "increment") {
-    return initialState + 1;
-  } else if (action === "decrement" && initialState > 0) {
-    return initialState - 1;
-  } else if (action === "reset") {
-    return 0;
-  } else {
-    return initialState;
+function reducer(state, action) {
+  switch (action.type) {
+    case "increment":
+      return { count: state.count + 1 };
+    case "decrement":
+      return { count: state.count - 1 };
+    case "reset":
+      return { count: 0 };
+    default:
+      return state;
   }
 }
 
 function Counter() {
-  const [count, dispatch] = useReducer(countReducer, 0);
+  const [state, dispatch] = useReducer(reducer, { count: 0 });
 
-  const [newCount, setNewCount] = useState("");
+  const [newCount, setNewCount] = useState(0);
+
+  function increment() {
+    dispatch({ type: "increment" });
+  }
+
+  function decrement() {
+    dispatch({ type: "decrement" });
+  }
+
+  function reset() {
+    dispatch({ type: "reset" });
+  }
 
   const handleChange = (event) => {
     event.preventDefault();
-    setNewCount(event.target.value);
+    setNewCount(Number(event.target.value));
   };
   const setValue = (event) => {
     event.preventDefault();
-    count([newCount]);
+    state.count = newCount;
     setNewCount("");
   };
 
   return (
     <div className="bg-neutral-200">
       <Helmet
-        title={`Counter is ${count}`}
+        title={`Counter is ${state.count}`}
         description="This is a simple counter app with useReducer."
       />
       <div className="flex gap-5 justify-center items-center h-20 mx-auto pt-5">
@@ -53,23 +66,23 @@ function Counter() {
       <div className="bg-neutral-200 w-full h-screen flex items-center">
         <div></div>
         <div className="flex flex-col p-4 max-w-lg h-96 mx-auto rounded-xl w-full justify-center items-center bg-gradient-to-bl from-red-700 via-pink-500 to-slate-500 shadow-xl">
-          <h1 className="text-3xl pb-3">Count: {count}</h1>
+          <h1 className="text-3xl pb-3">Count: {state.count}</h1>
           <div className="flex flex-row flex-wrap flex-initial p-4 justify-around">
             <button
               className="bg-gradient-to-tr from-lime-500 to-rose-300 p-3 rounded-lg shadow-md hover:bg-slate-600 duration-300 hover:scale-105"
-              onClick={() => dispatch("increment")}
+              onClick={increment}
             >
               Increment
             </button>
             <button
               className="bg-gradient-to-tr from-lime-500 to-rose-300 p-3 rounded-lg shadow-md hover:bg-slate-600 duration-300 hover:scale-105"
-              onClick={() => dispatch("decrement")}
+              onClick={decrement}
             >
               Decrement
             </button>
             <button
               className="bg-gradient-to-tr from-lime-500 to-rose-300 p-3 rounded-lg shadow-md hover:bg-slate-600 duration-300 hover:scale-105"
-              onClick={() => dispatch("reset")}
+              onClick={reset}
             >
               reset
             </button>
